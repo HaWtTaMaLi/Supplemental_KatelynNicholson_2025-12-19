be147502eea010a7d4b75b6a494022dc2203c5a1
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-12-19 13:49:29 -0400

    push

--- Assets/Scrpits/PlayerMovement.cs
+++ Assets/Scrpits/PlayerMovement.cs
@@ -2,15 +2,17 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private CharacterController Controller;
+    public CharacterController Controller;
+    public int speed = 5;
 
     void Start()
     {
-        
+        Controller = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        Controller.Move(move * Time.deltaTime * speed);
     }
 }
