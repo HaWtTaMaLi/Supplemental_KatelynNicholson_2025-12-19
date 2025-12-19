cbfce0b04ec37a537198b2541a3a76772b5ee9fb
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-12-19 15:15:50 -0400

    push

--- Assets/Scrpits/PlayerMovement.cs
+++ Assets/Scrpits/PlayerMovement.cs
@@ -3,16 +3,33 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public CharacterController Controller;
+    public Transform Camera;
+
     public int speed = 5;
+    public float mouseSensitivity = 50f;
+    public float xRotation = 0f;
+    public float upClamp = -90f;
+    public float downClamp = 30f;
 
     void Start()
     {
         Controller = GetComponent<CharacterController>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     void Update()
     {
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        Controller.Move(move * Time.deltaTime * speed);
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, upClamp, downClamp);
+        Camera.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        Vector3 move = transform.right * Input.GetAxis("Horizontal") + transform.forward * Input.GetAxis("Vertical");
+        Controller.Move(move * Time.deltaTime * speed); 
     }
 }
