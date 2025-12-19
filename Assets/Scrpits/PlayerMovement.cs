63138c28d47802bc907751191cd8fb182f736489
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-12-19 13:15:47 -0400

    push

---
+++ Assets/Scrpits/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController Controller;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
