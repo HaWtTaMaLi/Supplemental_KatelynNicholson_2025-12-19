72b7bb6f0a7da1b9c9b623d8ef5687267d0392e8
Mali <87450284+HaWtTaMaLi@users.noreply.github.com>
2025-12-22 10:32:43 -0400

    push

---
+++ Assets/RadioTrig.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class RadioTrig : MonoBehaviour
+{
+    public AudioSource radioSound;
+    public bool isPlaying = false;
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            if (!isPlaying)
+            {
+                radioSound.Play();
+                isPlaying = true;
+            }
+            else
+            {
+                radioSound.Stop();
+                isPlaying = false;
+            }
+        }
+    }
+}
